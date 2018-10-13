cf6b31dc3b706090bb76b55dc63c7135368d175b
Egor Chernyshov <jarogor@gmail.com>
2018-10-13 20:47:07 +0300

    Декоратор замера времени работы функции

--- decorators.py
+++ decorators.py
@@ -1,12 +1,28 @@
-def input_number(callback):
+from time import time
+
+
+def input_number(fn):
     """
     Декоратор обработки ввода числа
     """
 
     def wrap(input_value, message_error):
         try:
-            return callback(int(input_value))
+            return fn(int(input_value))
         except ValueError:
             return message_error
+
+    return wrap
+
+
+def timeit(fn):
+    """
+    Декоратор замера времени работы функции
+    """
+
+    def wrap(*args):
+        start_time = time()
+        fn(*args)
+        print(f'Время выполнения функци: {time() - start_time}')
 
     return wrap

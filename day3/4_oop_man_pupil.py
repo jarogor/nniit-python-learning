b336d4969dfc0e89d2c03f1f9b2ca515674b8547
Egor Chernyshov <jarogor@gmail.com>
2018-10-13 16:43:20 +0300

    Создание класса Man и наследника Pupil

--- day3/4_oop_man_pupil.py
+++ day3/4_oop_man_pupil.py
@@ -3,22 +3,22 @@ from random import randint
 
 
 class Man:
-    name = "<Unknown>"
+    first_name = "<Unknown>"
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, first_name):
+        self.first_name = first_name
 
     def solve_task(self):
-        print(f"{self.name} said:")
+        print(f"{self.first_name} said:")
         print("I'm not ready yet")
 
-    def get_name(self):
-        return self.name
+    def get_first_name(self):
+        return self.first_name
 
 
 class Pupil(Man):
-    def __init__(self, name):
-        super().__init__(name)
+    def __init__(self, first_name):
+        super().__init__(first_name)
 
     def solve_task(self):
         print('thinking...')
